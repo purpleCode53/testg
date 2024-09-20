cfa0db6915ff6f999a8a50ca84ac2231423eb9d5
purplecode53 <purplecode53@gmail.com>
2024-09-20 21:17:22 +0900

    test

---
+++ Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.toy.Ingstaram.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello ingstar";
+    }
+}
